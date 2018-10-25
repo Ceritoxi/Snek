5a297e99fcd5d815b5903d79a07ef9fb0637c69a
ceritoxi <ceritoxi@gmail.com>
2018-10-25 22:04:46 +0200

    install.sh contained some highly unstable and probably unnecessary code, refactored for the betterment of mankind

--- install.sh
+++ install.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Make bin directory\n";
-mkdir -p bin;
-cd bin;
-bin_path=`echo $PWD`;
-cd ../source;
-source_path=`echo $PWD`;
-
-echo "Finding javac\n";
-javac_find=`find / -name javac 2> /dev/null`;
-javac_paths=(${javac_find// / });
-
-echo "PLEASE, IGNORE WARNINGS!";
-echo "Trying to call javac";
-
-for i in "${!javac_paths[@]}"
-do
-	${javac_paths[i]} -d $bin_path $source_path/*.java;
-done
+mkdir bin
+javac -d bin source/*
